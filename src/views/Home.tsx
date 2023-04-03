8d84397a9e2fb00de574acf98cb7cdd3b07c6609
Barry Percy <baz.percy@gmail.com>
2023-04-03 20:23:04 +0100

    Created a button that functions with the api

---
+++ src/views/Home.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container,Button } from "react-bootstrap";
+import "./styles.css";
+
+const Home = () => {
+    const getAPI = async ()=>{
+        try {
+            const imdbId = "tt3896198"
+            const response = await fetch(process.env.REACT_APP_BACKEND+"/movies/"+imdbId)
+            if(response.ok){
+                const movie = await response.json()
+                console.log(movie)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <Container fluid="sm">
+            <h1 className="blog-main-title">OMDb</h1>
+            <Button variant="primary" onClick={getAPI}>Get Movie</Button>
+        </Container>
+    );
+};
+
+export default Home;
