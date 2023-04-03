b49ba977ac2c13f026cce65f1f35ac1b6e52a4b3
Barry Percy <baz.percy@gmail.com>
2023-04-03 22:03:54 +0100

    Moved test get action to redux actions and removed button

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,22 +4,10 @@ import "./styles.css";
 import SearchBar from "../../components/SearchBar";
 
 const Home = () => {
-    const getAPI = async ()=>{
-        try {
-            const imdbId = "tt3896198"
-            const response = await fetch(process.env.REACT_APP_BACKEND+"/movies/"+imdbId)
-            if(response.ok){
-                const movie = await response.json()
-                console.log(movie)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    
     return (
         <Container fluid="sm">
             <h1 className="blog-main-title">OMDb</h1>
-            <Button variant="primary" onClick={getAPI}>Get Movie</Button>
             <SearchBar/>
         </Container>
     );
